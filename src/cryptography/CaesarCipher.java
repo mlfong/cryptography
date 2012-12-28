501ba6e2fa5d2d5cc61a5c16df50aa9b4bcf2b45
Man Fong <mlfong@gatech.edu>
2012-12-27 21:11:57 -0500

    implement caesar cipher

--- src/cryptography/CaesarCipher.java
+++ src/cryptography/CaesarCipher.java
@@ -3,6 +3,8 @@ package cryptography;
 public class CaesarCipher implements Cipher
 {
     private int rotation ;
+    private final int ASCII_MOD = 256;
+    
     public CaesarCipher(int r)
     {
         this.rotation = r;
@@ -11,21 +13,30 @@ public class CaesarCipher implements Cipher
     @Override
     public String encrypt(String message)
     {
-        // TODO Auto-generated method stub
-        return null;
+        StringBuilder sb = new StringBuilder(message.length());
+        for(Character c : message.toCharArray())
+            sb.append((char)((c+rotation)%ASCII_MOD));
+        return sb.toString();
     }
 
     @Override
     public String decrypt(String encoded)
     {
-        // TODO Auto-generated method stub
-        return null;
+        StringBuilder sb = new StringBuilder(encoded.length());
+        for(Character c : encoded.toCharArray())
+            sb.append((char)((c+256-rotation)%ASCII_MOD));
+        return sb.toString();
     }
 
     public static void main(String[] args)
     {
-        // TODO Auto-generated method stub
-
+        int rotation = 15;
+        CaesarCipher cc = new CaesarCipher(rotation);
+        String message = "Another great message!!101!1!";
+        String encrypt = cc.encrypt(message);
+        System.out.println(encrypt);
+        String decrypt = cc.decrypt(encrypt);
+        System.out.println(decrypt);
     }
 
 }
